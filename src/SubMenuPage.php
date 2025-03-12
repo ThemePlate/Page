bd41d69ca25e009a63ff6ed7e84c32020aa3238a
kermage <genealyson.torcende@gmail.com>
2025-03-12 23:48:05 +0800

    default empty parent

--- src/SubMenuPage.php
+++ src/SubMenuPage.php
@@ -13,7 +13,7 @@ use ThemePlate\Page\Interfaces\SubMenuPageInterface;
 
 class SubMenuPage extends BasePage implements SubMenuPageInterface {
 
-	public function __construct( string $title, string $parent_slug, array $config = array() ) {
+	public function __construct( string $title, string $parent_slug = '', array $config = array() ) {
 
 		if ( '' !== $parent_slug ) {
 			_deprecated_argument( __METHOD__, '2.1.0', 'Use the new ' . esc_html( self::class . '::parent()' ) . ' instead.' );
